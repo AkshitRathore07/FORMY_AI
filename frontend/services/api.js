d06b4756bcd59d96bd8c939b66e2b3863a53e077
AkshitRathore07 <akshitrathore55128@gmail.com>
2026-01-14 21:06:23 +0530

    Full setup

--- frontend/services/api.js
+++ frontend/services/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
+   
 export const createStudent = (data) => API.post("/students", data);
 export const getStudents = () => API.get("/students");
